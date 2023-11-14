18c93778711688a673ecc7c346f106ac33386946
Alex Munteanu <vootehdoo@gmail.com>
2023-11-14 10:18:44 +0100

    Add country flags

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,10 @@ const searchBtn = document.querySelector(".search-btn");
 const searchInput = document.querySelector(".search-input");
 const todayDate = document.querySelector(".date");
 const city = document.querySelector(".city");
-const date = document.querySelector(".date");
+const country = document.querySelector(".country");
 const currentTemperature = document.querySelector(".current-temperature");
+const flag = document.getElementById("flag");
+
 
 let lat;
 let long;
@@ -22,23 +24,19 @@ searchBtn.addEventListener("click", async (event) => {
   lat = locationData.results[0].latitude;
   long = locationData.results[0].longitude;
   console.log(locationData);
-  console.log(locationData.results[0].name);
-  console.log(lat);
-  console.log(long);
+
 
   const weatherApi = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current=temperature_2m,is_day&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min`;
   const weatherResponse = await fetch(weatherApi);
   const weatherData = await weatherResponse.json();
-  console.log(weatherData);
-  console.log(weatherData.current.temperature_2m);
+
 
   //update DOM values
-  city.textContent = `${locationData.results[0].name}`;
+  city.textContent = `${locationData.results[0].name}`; //city name
+  country.textContent = `${locationData.results[0].country}`; //country name
   currentTemperature.textContent = `${Math.round(weatherData.current.temperature_2m)}°C`;
-  date.textContent = `${weatherData.current.time}`;
-  //to be continued
-
-
+  flag.src = `https://flagsapi.com/${locationData.results[0].country_code}/flat/32.png`;
+  console.log(flag);
 
 });
 

eb1641ff742cc085ce8363f4085c969bb35891d1
Alex Munteanu <vootehdoo@gmail.com>
2023-11-14 16:37:07 +0100

    Clear the code in script.js

--- scripts/script.js
+++ scripts/script.js
@@ -16,11 +16,7 @@ const futureDate = document.querySelectorAll(".future-date");
 
 const hourlyWeather = document.querySelector(".hourly-weather");
 
-
 const main = document.querySelector("main");
-
-
-
 
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dec"];
 
@@ -38,19 +34,16 @@ searchBtn.addEventListener("click", async (event) => {
   //api's
   const locationApi = `https://geocoding-api.open-meteo.com/v1/search?name=${searchInput.value}&count=1&language=en&format=json`;
 
-
   const locationResponse = await fetch(locationApi);
   const locationData = await locationResponse.json();
   lat = locationData.results[0].latitude;
   long = locationData.results[0].longitude;
   console.log(locationData);
 
-
   const weatherApi = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current=temperature_2m,apparent_temperature,is_day,precipitation,rain,wind_speed_10m,wind_direction_10m,wind_gusts_10m&hourly=temperature_2m,relative_humidity_2m,dew_point_2m,apparent_temperature,precipitation_probability,precipitation,rain,showers,weather_code,cloud_cover,visibility,wind_speed_10m,wind_speed_80m,wind_speed_120m,wind_speed_180m,wind_direction_10m,wind_direction_80m,wind_direction_120m,wind_direction_180m,wind_gusts_10m&daily=weather_code,uv_index_max,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset,uv_index_max,uv_index_clear_sky_max,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,precipitation_probability_max`;
   const weatherResponse = await fetch(weatherApi);
   const weatherData = await weatherResponse.json();
   console.log(weatherData);
-
 
   //update DOM values
   city.textContent = `${locationData.results[0].name}`; //city name
@@ -123,10 +116,6 @@ searchBtn.addEventListener("click", async (event) => {
     hourlyDiv.append(hourlyTemp);
     hourlyTemp.textContent = `${Math.round(weatherData.hourly.temperature_2m[i])}°`;
 
-
   }
-
-
-
 });
 
